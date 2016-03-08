e27650ecb36ccbe40e0fe529dd891d16221d64fb
Anubha <anubhaaggarwal95@gmail.com>
2016-03-08 08:52:45 +0530

    Declare manifest permissions and Detect Camera

--- app/src/main/java/com/example/android/videorecorder/MainFragment.java
+++ app/src/main/java/com/example/android/videorecorder/MainFragment.java
@@ -1,8 +1,12 @@
 package com.example.android.videorecorder;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,5 +20,22 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fragment_main,container,false);
         return rootView;
+    }
+    // detecting a camera
+    private boolean checkCameraHardware(Context context)
+    {
+       return  context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+    //accessing cameras
+    public void getCameraInstance()
+    {
+        try
+        {
+            CameraManager cameraManager=(CameraManager)getActivity().getSystemService(Context.CAMERA_SERVICE);
+
+        }
+        catch(Exception e) {
+            Log.d("Accessing Camera ", e.toString());
+        }
     }
 }
