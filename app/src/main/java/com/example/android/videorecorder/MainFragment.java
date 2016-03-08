0bd5827a77753f9ff067a1b8697049ead8cf490d
Anubha <anubhaaggarwal95@gmail.com>
2016-03-08 19:17:25 +0530

    Access the Camera

--- app/src/main/java/com/example/android/videorecorder/MainFragment.java
+++ app/src/main/java/com/example/android/videorecorder/MainFragment.java
@@ -3,9 +3,8 @@ package com.example.android.videorecorder;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraManager;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +14,6 @@ import android.view.ViewGroup;
  * Created by User on 06-03-2016.
  */
 public class MainFragment extends Fragment {
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fragment_main,container,false);
@@ -27,15 +25,17 @@ public class MainFragment extends Fragment {
        return  context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
     //accessing cameras
-    public void getCameraInstance()
+    public static Camera getCameraInstance()
     {
+        Camera camera=null;
         try
         {
-            CameraManager cameraManager=(CameraManager)getActivity().getSystemService(Context.CAMERA_SERVICE);
-
+            camera=Camera.open();
+          //  Camera.Parameters parameters=camera.getParameters();
         }
         catch(Exception e) {
             Log.d("Accessing Camera ", e.toString());
         }
+        return camera;
     }
 }
